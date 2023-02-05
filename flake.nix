24e0af1928ada6c86970605b14a6726aa3dde287
Luis-Hebendanz <luis.nixos@gmail.com>
2023-02-05 13:05:50 +0100

    Packaged python

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+      pkgs = forAllSystems (system: nixpkgs.legacyPackages.${system});
+      myapp = forAllSystems (system: pkgs.${system}.poetry2nix.mkPoetryApplication { projectDir = self; });
+    in
+    rec {
+      packages = forAllSystems (system: {
+        default =
+          let
+            spkgs = pkgs.${system};
+            pypkgs = pkgs.${system}.python310Packages;
+          in
+          pypkgs.buildPythonPackage {
+            pname = "testbench";
+            version = "2.0";
+            src = self;
+
+            doCheck = false;
+            buildInputs =  with pypkgs; [ setuptools ];
+          };
+      });
+
+
+
+      devShells = forAllSystems (system: {
+        default =
+          let
+            spkgs = pkgs.${system};
+            pypkgs = pkgs.${system}.python310Packages;
+          in
+          pkgs.${system}.mkShellNoCC {
+          packages = with spkgs; [
+
+          ];
+        };
+      });
+    };
+}
