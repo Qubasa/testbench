84080a537095a1a1e790120eefe052357bfb15b2
Luis-Hebendanz <luis.nixos@gmail.com>
2023-02-22 11:24:20 +0100

    Package correction

--- setup.py
+++ setup.py
@@ -3,12 +3,5 @@ import setuptools
 setuptools.setup(
     name='testbench',
     version = "2.0",
-    author='Sebastian Bräuer',
-    author_email='braeuer@tkn.tu-berlin.de',
-    description='TU Berlin RNVS course testbenches',
-    # entry_points={
-    #     'console_scripts': [
-    #         'testbench = testbench.block6:main',
-    #     ]
-    # }
+    description='Rechnernetze test utils',
 )
